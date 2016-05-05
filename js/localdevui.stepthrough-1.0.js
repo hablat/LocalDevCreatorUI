a731b0b6664ab352026ba59cf1598c72324073e9
hablat <hablat@jbsinternational.com>
2016-05-04 20:56:16 -0400

    stepthrough javascript and new step

--- js/localdevui.stepthrough-1.0.js
+++ js/localdevui.stepthrough-1.0.js
@@ -1,3 +1,36 @@
 /**
  * Created by hablat on 5/4/2016.
  */
+var step = 1;
+
+function next(e) {
+    var value = e.value;
+
+    if(value == 'drupal') {
+        //alert("DRUPAL");
+        step = 2;
+    } else if(value = 'moodle') {
+        //alert("MOODLE");
+        step = 2;
+    }
+
+    if(step > 1) {
+        document.getElementById('prev-button').style.display = "block";
+    }
+
+    document.getElementById('step' + (step - 1)).style.display = "none";
+    document.getElementById('step' + step).style.display = "block";
+}
+
+function prev() {
+    step--;
+
+    if(step >= 1) {
+        document.getElementById('step' + (step + 1)).style.display = "none";
+        document.getElementById('step' + step).style.display = "block";
+
+        if(step == 1) {
+            document.getElementById('prev-button').style.display = "none";
+        }
+    }
+}

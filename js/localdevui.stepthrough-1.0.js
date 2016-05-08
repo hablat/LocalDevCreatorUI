ff958f2144bc7178ff21c8345a7764c04d62f9a9
hablat <hablat@jbsinternational.com>
2016-05-08 16:37:07 -0400

    5/8/2016 step 2 changes for validation

--- js/localdevui.stepthrough-1.0.js
+++ js/localdevui.stepthrough-1.0.js
@@ -1,37 +1,57 @@
 /**
  * Created by hablat on 5/4/2016.
  */
-var step = 1;
+var nextStep = 1;
 
 function next(e) {
     var value = e.value;
+    var stacksSelected = [];
 
     if(value == 'drupal') {
-        //alert("DRUPAL");
-        step = 2;
+        nextStep = 2;
     } else if(value == 'moodle') {
-        //alert("MOODLE");
-        step = 2;
+        nextStep = 2;
     } else if(value == 'stack') {
-        alert('STACK BITCH!');
+        var stacksChecked = document.getElementsByName('stack-checkbox');
+
+        for(var i = 0; i < stacksChecked.length; i++) {
+            if(stacksChecked[i].checked) { //checked
+                if(stacksSelected.indexOf(stacksChecked[i].value) < 0) { // If it's not already added
+                    stacksSelected.push(stacksChecked[i].value); // add
+                }
+            } else { // unchecked
+                var index = stacksSelected.indexOf(stacksChecked[i].value);
+
+                if(index >= 0) { // in list of checked stacks
+                    stacksSelected.splice(index, 1); // Remove stack from list of checked
+                }
+            }
+        }
+
+        if(stacksSelected.length > 0) { // if stacks are selected continue
+            alert('changing page')
+            nextStep = 3;
+        } else { // else alert
+            alert('Please Select a stack before continuing');
+        }
     }
 
-    if(step > 1) {
+    if(nextStep > 1) {
         document.getElementById('prev-button').style.display = "block";
     }
 
-    document.getElementById('step' + (step - 1)).style.display = "none";
-    document.getElementById('step' + step).style.display = "block";
+    document.getElementById('step' + (nextStep - 1)).style.display = "none";
+    document.getElementById('step' + nextStep).style.display = "block";
 }
 
 function prev() {
-    step--;
+    nextStep--;
 
-    if(step >= 1) {
-        document.getElementById('step' + (step + 1)).style.display = "none";
-        document.getElementById('step' + step).style.display = "block";
+    if(nextStep >= 1) {
+        document.getElementById('step' + (nextStep + 1)).style.display = "none";
+        document.getElementById('step' + nextStep).style.display = "block";
 
-        if(step == 1) {
+        if(nextStep == 1) {
             document.getElementById('prev-button').style.display = "none";
         }
     }
